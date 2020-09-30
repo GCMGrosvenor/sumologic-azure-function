73db5345525d6393add7bb1b9d3eaafb186f4395
Haim Izrael <hizrael@gcmlp.com>
2020-09-30 10:58:12 -0500

    add unit tests

--- EventHubs/src/EventHubs_Logs/lib/moduledatatransformer.js
+++ EventHubs/src/EventHubs_Logs/lib/moduledatatransformer.js
@@ -6,19 +6,20 @@ var rgMap = {}; // map of resource group name -> module name
 module.exports = function (subscriptionId = process.env.AZURE_SUBSCRIPTION_ID) {
 
     var msRestNodeAuth = require('@azure/ms-rest-nodeauth');
-    const { ResourceManagementClient, ResourceManagementModels, ResourceManagementMappers } = require('@azure/arm-resources');
+    const { ResourceManagementClient } = require('@azure/arm-resources');
 
     async function getClient() {        
-        var creds = await msRestNodeAuth.loginWithAppServiceMSI({tokenCache: tokenCache});
+        const creds = await msRestNodeAuth.loginWithAppServiceMSI({tokenCache: tokenCache});
         return new ResourceManagementClient(creds, subscriptionId);
     }
 
     async function getResourceGroupModule(rgName) {
-        var client = await getClient();
+        const client = await getClient();
         try {
-            var rg = await client.resourceGroups.get(rgName);
+            const rg = await client.resourceGroups.get(rgName);
             return rg && rg.tags && rg.tags.module ? rg.tags.module : undefined;
         } catch (error) {
+            console.error(`${error}`);
             return undefined;
         }
     }
@@ -28,8 +29,8 @@ module.exports = function (subscriptionId = process.env.AZURE_SUBSCRIPTION_ID) {
             return rgMap[rgName];
         }
 
-        var moduleName = await getResourceGroupModule(rgName);
-        console.log(`Looked up module name for ResourceGroup ${rgName}=${moduleName}`);        
+        const moduleName = await getResourceGroupModule(rgName);
+        console.log(`Looked up module name for ResourceGroup ${rgName}=${moduleName}`);
         if (moduleName != undefined) {
             rgMap[rgName] = moduleName;
         }
@@ -37,11 +38,12 @@ module.exports = function (subscriptionId = process.env.AZURE_SUBSCRIPTION_ID) {
     }
 
     return {
-        transform: async function(msg) {
-            var rgName = msg.resourceGroupName;
+        transform: async function (msg) {
+            if (msg === undefined || msg === null) return msg;
+            const rgName = msg.resourceGroupName;
             if (!rgName) return msg;
 
-            var moduleName = await getModuleNameFor(rgName);
+            const moduleName = await getModuleNameFor(rgName);
             if (moduleName) {
                 msg.assetmodule = moduleName;
             }
